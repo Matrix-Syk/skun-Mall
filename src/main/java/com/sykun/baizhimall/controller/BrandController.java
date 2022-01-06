274c220a68f5c08a9b93732671f504d46a501a51
Matrix-Syk <91580471+Matrix-Syk@users.noreply.github.com>
2022-01-06 16:24:52 +0800

    Update BrandController.java

--- src/main/java/com/sykun/baizhimall/controller/BrandController.java
+++ src/main/java/com/sykun/baizhimall/controller/BrandController.java
@@ -46,11 +46,11 @@ public class BrandController {
     @Resource
     private CategoryBrandRelationService relationService;
     // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-    private final String endpoint = "https://oss-cn-beijing.aliyuncs.com";
+    private final String endpoint = "";
     // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-    private final String accessKeyId = "LTAI5tR";
-    private final String accessKeySecret = "2dfXF7";
-    private final String bucketName = "sy";
+    private final String accessKeyId = "";
+    private final String accessKeySecret = "";
+    private final String bucketName = "";
 
     @PostMapping("/list")
     public Map<String, Object> showBrand(@RequestBody BrandEntity brandEntity) {
